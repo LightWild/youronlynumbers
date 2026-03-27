d834ca94c261c842d36d2b70af9d4afb55c63e18
灯野科技 <shilinejade@outlook.com>
2026-03-27 22:21:30 +0800

    Update submit.js

--- api/submit.js
+++ api/submit.js
@@ -16,8 +16,8 @@ export default async function handler(req, res) {
     }
 
     // 2. 读取 Gist 数据
-    const GIST_ID = "你的GistID"; // 替换成你的 Gist ID
-    const GIST_TOKEN = "你的GitHubToken"; // 替换成你的 Token
+    const GIST_ID = "fe73c32ddd8ad4ec118743edc3fcfd02"; // 替换成你的 Gist ID
+    const GIST_TOKEN = "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"; // 替换成你的 Token
     const GIST_FILENAME = "data.json"; // 你的数据文件名
 
     const gistRes = await fetch(`https://api.github.com/gists/${GIST_ID}`, {
